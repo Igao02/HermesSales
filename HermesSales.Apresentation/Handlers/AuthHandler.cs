b66e8c1d6640a1731344d4734099d3518c693917
Igor Abiezer Lima de Oliveira <gordinis213@outlook.com>
2026-03-07 01:15:37 -0300

    Primeira versão para cadastrar o usuario

---
+++ HermesSales.Apresentation/Handlers/AuthHandler.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity.Data;
+using static HermesSales.Apresentation.Components.Pages.Account.User.Register.Index;
+
+namespace HermesSales.Apresentation.Handlers;
+
+public class AuthHandler
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public AuthHandler(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<bool> Register(RegisterModel model)
+    {
+        var client = _httpClientFactory.CreateClient("ApiBack");
+
+        var request = new RegisterRequest
+        {
+            Email = model.Email,
+            Password = model.Password
+        };
+
+        var response = await client.PostAsJsonAsync("/register", request);
+
+        var content = await response.Content.ReadAsStringAsync();
+
+        Console.WriteLine("STATUS: " + response.StatusCode);
+        Console.WriteLine("BODY: " + content);
+
+        return response.IsSuccessStatusCode;
+    }
+}
---
+++ HermesSales.Apresentation/Program.cs
@@ -0,0 +1,40 @@
+using HermesSales.Apresentation.Components;
+using HermesSales.Apresentation.Handlers;
+using MudBlazor.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpClient("ApiBack", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7238");
+});
+
+// Add MudBlazor services
+builder.Services.AddMudServices();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddScoped<AuthHandler>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
